ab91dcad430d9b3f676b48b1506accfbd4ce4f04
Ruth Bakare <ruth.bakare@mymona.uwi.edu>
2024-10-27 23:12:04 -0500

    Added functionality to restart the game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,14 +3,21 @@ document.addEventListener("DOMContentLoaded", function(){
     let thisPlayer='X';
     //Initializing array to keep game state
     const stateofgame= Array(9).fill(null); 
-    squares.forEach(square => {
+    const status= document.getElementById("status")
+    squares.forEach((square, index) => {
         square.classList.add("square");
         square.addEventListener('click',function(){
             if(!stateofgame[index]){
                 stateofgame[index]=thisPlayer;
                 square.textContent= thisPlayer;
                 square.classList.add(thisPlayer);
-                thisPlayer= thisPlayer==='X'?'O':'X';
+        
+                if(checkthewinner()){
+                    status.textContent = `Congratulations! ${thisPlayer} is the Winner!`;
+                    status.classList.add("you-won");
+                }else{
+                    thisPlayer= thisPlayer==='X'?'O':'X';
+                }
             }
         });
         //Changes the look of the square whenever a user moves their mouse over a square 
@@ -22,8 +29,38 @@ document.addEventListener("DOMContentLoaded", function(){
         //Return square to the original style when the user's mouse leaves the square
         square.addEventListener("mouseout", function(){
             square.classList.remove("hover");
-        })
+        });
     });
+    function checkthewinner(){
+        const winningSequences=[
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+        ];
+        return winningSequences.some((seq) => {
+            return(
+                stateofgame[seq[0]]== thisPlayer &&
+                stateofgame[seq[1]]== thisPlayer &&
+                stateofgame[seq[2]]== thisPlayer 
+            );
+        });
+    }
+    function restartGame(){
+        stateofgame.fill(null);
+        squares.forEach((square)=> {
+            square.textContent="";
+            square.classList.remove('X','O','hover');
+        });
+        thisPlayer='X';
+        status.textContent="Move your mouse over a square and click to play an X or an O."
+        status.classList.remove("you-won");
+    }
+    NewGameButton.addEventListener("click",restartGame);
 
 });
 
