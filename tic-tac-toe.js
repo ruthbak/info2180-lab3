663c00ad6077d91a3608a8e811862a8f84ca5eb0
Ruth Bakare <ruth.bakare@mymona.uwi.edu>
2024-10-27 21:12:56 -0500

    Added an X or O to a aquare when clicked, Change the style when user moves mouse over a square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
     const squares=document.querySelectorAll('#board div');
+    let thisPlayer='X';
+    //Initializing array to keep game state
+    const stateofgame= Array(9).fill(null); 
     squares.forEach(square => {
         square.classList.add("square");
+        square.addEventListener('click',function(){
+            if(!stateofgame[index]){
+                stateofgame[index]=thisPlayer;
+                square.textContent= thisPlayer;
+                square.classList.add(thisPlayer);
+                thisPlayer= thisPlayer==='X'?'O':'X';
+            }
+        });
+        //Changes the look of the square whenever a user moves their mouse over a square 
+        square.addEventListener("mouseover", function(){
+            if(!stateofgame[index]){
+                square.classList.add("hover");
+            }
+        });
+        //Return square to the original style when the user's mouse leaves the square
+        square.addEventListener("mouseout", function(){
+            square.classList.remove("hover");
+        })
     });
 
 });
